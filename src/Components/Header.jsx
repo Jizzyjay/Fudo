30e9d0a9bbfac89bb6660012314478364a04974b
Ekunola John Oluwatosin <ekunolajohn@gmail.com>
2022-07-28 12:36:59 +0100

    LOADER

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -135,15 +135,16 @@ const Header = () => {
                                 {
                                     user && user.email === "jizzyjay423@gmail.com" && (
                                         <Link to={'./createItem'}>
-                                            <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 text-textColor text-base'>New Item <MdAdd /></p>
+                                            <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 text-textColor text-base'
+                                            onClick={() => setIsMenu(false)}>New Item <MdAdd /></p>
                                         </Link>
                             )}
 
                     <ul className='flex flex-col px-4 py-2'>
-                        <li className='text-base text-textColor cursor-pointer hover:text-headingColor transition-all ease-in-out  duration-100 hover:bg-slate-100 px-4 py-2'>Home</li>
-                        <li className='text-base text-textColor cursor-pointer hover:text-headingColor transition-all ease-in-out duration-100 hover:bg-slate-100 px-4 py-2'>Menu</li>
-                        <li className='text-base text-textColor cursor-pointer hover:text-headingColor transition-all ease-in-out duration-100 hover:bg-slate-100 px-4 py-2'>About Us</li>
-                        <li className='text-base text-textColor cursor-pointer hover:text-headingColor transition-all ease-in-out duration-100 hover:bg-slate-100 px-4 py-2'>Services</li>
+                        <li className='text-base text-textColor cursor-pointer hover:text-headingColor transition-all ease-in-out  duration-100 hover:bg-slate-100 px-4 py-2'onClick={() => setIsMenu(false)}>Home</li>
+                        <li className='text-base text-textColor cursor-pointer hover:text-headingColor transition-all ease-in-out duration-100 hover:bg-slate-100 px-4 py-2'onClick={() => setIsMenu(false)}>Menu</li>
+                        <li className='text-base text-textColor cursor-pointer hover:text-headingColor transition-all ease-in-out duration-100 hover:bg-slate-100 px-4 py-2'onClick={() => setIsMenu(false)}>About Us</li>
+                        <li className='text-base text-textColor cursor-pointer hover:text-headingColor transition-all ease-in-out duration-100 hover:bg-slate-100 px-4 py-2'onClick={() => setIsMenu(false)}>Services</li>
                     </ul>
                         <p className='m-2 p-2 rounded-md shadow-md flex items-center gap-3 cursor-pointer hover:bg-gray-100 transition-all duration-100 bg-slate-200 text-textColor text-base'
                             onClick={logout}
--- src/utilities/data.js
+++ src/utilities/data.js
@@ -3,6 +3,13 @@ import Icecream from '../asset/img/i1.png'
 import Strawberry from '../asset/img/f1.png'
 import Chicken from '../asset/img/c3.png'
 import Fish from '../asset/img/fi4.png'
+import Nugget from '../asset/img/c4.png'
+import Prawns from '../asset/img/cu3.png'
+import Vanila from '../asset/img/i3.png'
+import Rice from '../asset/img/r5.png'
+import Berries from '../asset/img/f4.png'
+import Monster from '../asset/img/d4.png'
+
 
 
 export const heroData = [
@@ -29,5 +36,50 @@ export const heroData = [
         name: 'Chicken Plate',
         price: '4500',
         imageSrc: Fish
+    },
+];
+
+export const categories = [
+        {
+        id: 1,
+        name: 'Chicken Nugget',
+        price: '2550',
+        urlParamName: 'chicken nuggets',
+        imageSrc: Nugget
+    },
+    {
+        id: 2,
+        name: 'Creamy Garlic Prawns',
+        price: '8500',
+        urlParamName: 'creay prawns',
+        imageSrc: Prawns
+    },
+    {
+        id: 3,
+        name: 'Vanila IceCream',
+        price: '2500',
+        urlParamName: 'vanila',
+        imageSrc: Vanila
+    },
+    {
+        id: 4,
+        name: 'Rice',
+        price: '4500',
+        urlParamName: 'rice',
+        imageSrc: Rice
+    },
+    {
+        id: 5,
+        name: 'Black Berries',
+        price: '4500',
+        urlParamName: 'black berries',
+        imageSrc: Berries
+    },
+    {
+        id: 6,
+        name: 'Monster Drink',
+        price: '4500',
+        urlParamName: 'monster drink',
+        imageSrc: Monster
     }
 ]
