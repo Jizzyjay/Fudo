69a78f786a4900c11e5e6538b201f3b27dd6799c
Ekunola John Oluwatosin <ekunolajohn@gmail.com>
2022-08-09 17:39:54 +0100

    build

--- src/Components/CartContainer.jsx
+++ src/Components/CartContainer.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { MdOutlineKeyboardBackspace } from 'react-icons/md'
 import { motion  } from 'framer-motion'
 import { RiRefreshFill } from 'react-icons/ri'
-import { BiMinus, BiPlus } from 'react-icons/bi'
 import { actionType } from '../context/reducer'
 import { useStateValue } from '../context/StateProvider'
 import EmptyCart from '../asset/img/emptyCart.svg'
 import CartItem from './CartItem'
 
-const CartContainer = () => {
-
+const CartContainer = ({ car}) => {
     const [{ cartShow, cartItems, user }, dispatch] = useStateValue();
+    const [flag, setFlag] = useState(1);
+    const [tot, setTot] = useState(0);
     
     const showCart = ()  => {
         dispatch({
@@ -18,6 +18,25 @@ const CartContainer = () => {
             cartShow: !cartShow
         })
     }
+
+    useEffect(() => {
+    let totalPrice = cartItems.reduce(function (accumulator, item) {
+      return accumulator + item.qty * item.price;
+    }, 0);
+        setTot(totalPrice);
+        console.log(tot);
+    }, [tot, flag]);
+
+    const clearCart = () => {
+    dispatch({
+        type: actionType.SET_CART,
+        cartItems: [],
+        });
+
+        localStorage.setItem("cartItems", JSON.stringify([]));
+    };
+
+    
 
     return (
         <motion.div 
@@ -33,96 +52,72 @@ const CartContainer = () => {
                     <MdOutlineKeyboardBackspace className='text-textColor text-3xl' />
                 </motion.div>
 
-                    <p
-                    className='text-textColor text-lg font-semibold'>Cart</p>
-                    <motion.p 
+                <p className='text-textColor text-lg font-semibold'>Cart</p>
+
+                <motion.p 
                     whileTap={{ scale: .75 }}
                     className='flex items-center gap-2 p-1 px-2 my-2 bg-gray-100 rounded-md
-                    hover:shadow-md cursor-pointer text-textColor text-base 
-                    '>Clear <RiRefreshFill />{" "}</motion.p>
+                        hover:shadow-md cursor-pointer text-textColor text-base'
+                    onClick={clearCart}
+                        >Clear <RiRefreshFill />
+                </motion.p>
             </div>
+
             {
-                cartItems && cartItems.length > 0 ? (
-            
-            <div className='w-full h-full bg-cartBg rounded-t-[2rem] flex flex-col'>
-                <div className='w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none'>
-                
-                    {
-                        cartItems && 
-                            cartItems.map(item => (
-                            
-                            <div 
-                                key={item.id}
-                                className='w-full p-1 px-2 rounded-lg bg-cartItem flex items-center gap-2'>
-                                <img 
-                                    src={item?.imageURL} 
-                                    alt=''
-                                    className='w-20 h-20 max-w-[60px] rounded-full object-contain'/>
+                cartItems && 
+                    cartItems.length > 0 ? (
+                        <div className='w-full h-full bg-cartBg rounded-t-[2rem] flex flex-col'>
+                            <div className='w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll scrollbar-none'>
+                                {
+                                    cartItems && 
+                                        cartItems.length > 0 &&
+                                        cartItems.map(item => (
+                                            <CartItem 
+                                                key={item.id}
+                                                item={item}
+                                                setFlag={setFlag}
+                                                flag={flag}
+                                            />
+                                    ))}
+                            </div>
 
-                        <div className='flex flex-col gap-2'>
-                            <p className='text-base text-gray-50'>{item?.title}</p>
-                            <p className='text-sm block text-gray-300 font-semibold'>{item?.price}</p>
-                        </div>
+                            <div className='w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2'>
+                                <div className='w-full flex items-center justify-between'>
+                                    <p className='text-gray-400 text-lg'>Sub Total</p>
+                                    <p className='text-gray-400 text-lg'>${tot}</p>
+                                </div>
 
-                        <div className='group flex items-center gap-2 ml-auto cursor-pointer'>
-                            <motion.div
-                                whileTap={{ scale: .75}}>
-                                <BiMinus className='text-gray-50' />
-                            </motion.div>
+                                <div className='w-full flex items-center justify-between'>
+                                    <p className='text-gray-400 text-lg'>Delivery</p>
+                                    <p className='text-gray-400 text-lg'>#1,500</p>
+                                </div>
+                                <div className='w-full border-b border-gray-600 my-2'></div>
+                                <div className='w-full flex items-center justify-between'>
+                                    <p className='text-gray-200 text-xl font-semibold'>Total</p>
+                                    <p className='text-gray-200 text-xl font-semibold'>#{tot + 2000}</p>
+                                </div>
 
-                            <p className='w-5 h-5 rounded-sm bg-cartBg text-gray-50 flex items-center justify-center'>{item?.qty}</p>
+                                {
+                                    user ? (
+                                        <motion.button
+                                            whileTap={{ scale: .8 }}
+                                            type='button'
+                                            className='w-full p-2 rounded-full bg-gradient-to-tr from-orange-400 to-orange-600 text-gray-50 text-lg my-2
+                                            hover:shadow-lg'>
+                                                Check Out
+                                        </motion.button>
+                                    ) : (
+                                        <motion.button
+                                            whileTap={{ scale: .8 }}
+                                            type='button'
+                                            className='w-full p-2 rounded-full bg-gradient-to-tr from-orange-400 to-orange-600 text-gray-50 text-lg my-2
+                                            hover:shadow-lg'>
+                                                Login to Check Out
+                                        </motion.button>
+                                    )
+                                }
 
-                            <motion.div
-                                whileTap={{ scale: .75}}>
-                                <BiPlus className='text-gray-50' />
-                            </motion.div>
-                        </div>
-                    </div>
-                        ))
-                        }
-                </div>
-
-                <div className='w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2'>
-                    <div className='w-full flex items-center justify-between'>
-                        <p className='text-gray-400 text-lg'>Sub Total</p>
-                        <p className='text-gray-400 text-lg'>#3000</p>
-                    </div>
-
-                    <div className='w-full flex items-center justify-between'>
-                        <p className='text-gray-400 text-lg'>Sub Total</p>
-                        <p className='text-gray-400 text-lg'>#3000</p>
-                    </div>
-
-                    <div className='w-full border-b border-gray-600 my-2'>
-
-                    </div>
-
-                    <div className='w-full flex items-center justify-between'>
-                        <p className='text-gray-200 text-xl font-semibold'>Total</p>
-                        <p className='text-gray-200 text-xl font-semibold'>#39,000</p>
-                    </div>
-
-                    {
-                        user ? (
-                            <motion.button
-                                whileTap={{ scale: .8 }}
-                                type='button'
-                                className='w-full p-2 rounded-full bg-gradient-to-tr from-orange-400 to-orange-600 text-gray-50 text-lg my-2
-                                hover:shadow-lg'>
-                                    Check Out
-                            </motion.button>
-                        ) : (
-                            <motion.button
-                                whileTap={{ scale: .8 }}
-                                type='button'
-                                className='w-full p-2 rounded-full bg-gradient-to-tr from-orange-400 to-orange-600 text-gray-50 text-lg my-2
-                                hover:shadow-lg'>
-                                    Login to Check Out
-                            </motion.button>
-                        )
-                    }
-
-                </div>
+                            </div>
             </div>
             ) : (
                 <div className='w-full h-full flex flex-col items-center justify-center gap-6'>
